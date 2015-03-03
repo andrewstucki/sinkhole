2a9a5c4afc9680e0bfca1a4cb7d41a15b106d447
Andrew Stucki <andrew.stucki@govdelivery.com>
2015-03-02 23:50:56 -0600

    Move the specs to match lib structure

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,7 @@ require 'simplecov'
 SimpleCov.start
 
 $:.unshift File.join(File.expand_path(File.join(__FILE__,'..','..')), 'lib')
-Dir["./spec/support/**/*.rb"].sort.each { |f| require f}
+Dir["./spec/*/support/**/*.rb"].sort.each { |f| require f}
 
 RSpec.configure do |config|
   config.mock_with :mocha
